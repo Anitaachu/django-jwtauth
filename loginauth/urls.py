ed40dc81763949e359a283f0fefc915286bd9200
Anita Achu <anitatom20@gmail.com>
2023-10-10 20:13:01 +0100

    Update urls.py
    Added retrieve user url

--- loginauth/urls.py
+++ loginauth/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     path("register/", RegisterView.as_view(), name="register"),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('user', views.AuthUserAPIView.as_view(), name="user")
 
 ]
